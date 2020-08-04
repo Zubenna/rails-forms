b43721d9835b40bf262c836901bbb1c553d25467
Adrian <adfpizarro@gmail.com>
2020-08-04 11:46:58 -0600

    Update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
-    def new
-    end
     def create
+  	#@user = User.new(user_params)
+	@user = User.new(whitelisted_user_params)
+  	if @user.save
+    		redirect_to new_user_path
+  	else
+    		render :new
+  	end
+    end
+
+   private
+
+    def whitelisted_user_params
+      params.require(:user).permit(:username,:email,:password)
+    end
+    def new
+	@user = User.new
     end
 end
