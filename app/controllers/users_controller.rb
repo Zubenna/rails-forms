ba9d6c11549fa293c323046bb4d3198c85c50146
Adrian <adfpizarro@gmail.com>
2020-08-04 16:07:55 -0600

    Add rubocop, add higligth feature on errors to the text fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def create
-  	@user = User.new(user_params)
-	# @user = User.new(whitelisted_user_params)
-  	if @user.save
-    		redirect_to new_user_path
-  	else
-    		render :new
-  	end
+  def create
+    @user = User.new(user_params)
+    # @user = User.new(whitelisted_user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def new
-        @user = User.new(email: "example@example.com")
-    end
+  def new
+    @user = User.new(email: 'example@example.com')
+  end
 
-    def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-    end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
 
-    def edit
-        @user = User.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-   private
+  private
 
-    def user_params
-      params.require(:user).permit(:username,:email,:password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
